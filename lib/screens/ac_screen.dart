c7a8cece71688bb33520bef96e1aa3fa127bb0d8
Sushant Neupane <nsushaant72@gmail.com>
2025-11-21 16:10:45 +0545

    worked more on ui

--- lib/screens/ac_screen.dart
+++ lib/screens/ac_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class AcScreen extends StatefulWidget {
   const AcScreen({super.key});
@@ -8,6 +9,8 @@ class AcScreen extends StatefulWidget {
 }
 
 class _AcScreenState extends State<AcScreen> {
+  var radialValue = 15.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +32,77 @@ class _AcScreenState extends State<AcScreen> {
                         color: Color(0xff8285ee),
                       ),
                     ),
-                    const Text("Air Conditoner")
+                    const Text(
+                      "Air Conditoner",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(width: 35),
+                    SfRadialGauge(
+                      axes: <RadialAxis>[
+                        RadialAxis(
+                          minimum: 0,
+                          maximum: 40,
+                          startAngle: 90,
+                          endAngle: 450,
+                          showLabels: true,
+                          labelsPosition: ElementsPosition.outside,
+                          showTicks: false,
+                          radiusFactor: 0.8,
+                          interval: 10,
+                          showFirstLabel: false,
+                          labelFormat: '{value}',
+                          axisLineStyle: const AxisLineStyle(
+                            cornerStyle: CornerStyle.bothFlat,
+                            color: Colors.black12,
+                            thickness: 22,
+                          ),
+                          pointers: <GaugePointer>[
+                            RangePointer(
+                              value: radialValue,
+                              cornerStyle: CornerStyle.bothCurve,
+                              width: 22,
+                              sizeUnit: GaugeSizeUnit.logicalPixel,
+                              color: Color(0xff8285ee),
+                            ),
+                            MarkerPointer(
+                              value: radialValue,
+                              enableDragging: true,
+                              onValueChanged: (value) {
+                                setState(() {
+                                  radialValue = value;
+                                });
+                              },
+                              markerHeight: 35,
+                              markerWidth: 35,
+                              markerType: MarkerType.circle,
+                              color: Color(0xff8285ee),
+                              borderWidth: 2,
+                              borderColor: Colors.white54,
+                            ),
+                          ],
+                          annotations: [
+                            GaugeAnnotation(
+                              angle: 90,
+                              axisValue: 5,
+                              positionFactor: 0.1,
+                              widget: Column(
+                                mainAxisSize: .min,
+                                children: [
+                                  const Icon(
+                                    Icons.thermostat,
+                                    color: Color(0xff8285ee),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ],
